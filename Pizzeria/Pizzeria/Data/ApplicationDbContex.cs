a6c78f65e8f44825c9f99571fc1fb6245daf755f
Tomasz Florczyk <tomasz.florczyk98@gmail.com>
2022-11-13 22:04:39 +0100

    Added ingredient model, added connection to database

---
+++ Pizzeria/Pizzeria/Data/ApplicationDbContex.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pizzeria.Data
+{
+    public class ApplicationDbContex : DbContext
+    {
+        public ApplicationDbContex(DbContextOptions options) : base(options) 
+        { 
+        }
+
+        public DbSet<Ingredient>? Ingredients { get; set; }
+    }
+}

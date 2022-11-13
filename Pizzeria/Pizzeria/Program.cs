d42f2081c20134dac11e8ec982f8b1ef3abda330
Tomasz Florczyk <tomasz.florczyk98@gmail.com>
2022-11-13 23:04:03 +0100

    Small changes

--- Pizzeria/Pizzeria/Program.cs
+++ Pizzeria/Pizzeria/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Pizzeria.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContex>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddRazorPages();
 
 var app = builder.Build();

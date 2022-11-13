adf9d491d6aca24f8480266c656e2b5e2c277566
Tomasz Florczyk <tomasz.florczyk98@gmail.com>
2022-11-14 00:14:03 +0100

    Updated gitignore

--- Pizzeria/PizzeriaAPI/Program.cs
+++ Pizzeria/PizzeriaAPI/Program.cs
@@ -4,6 +4,7 @@ using PizzeriaAPI.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddRazorPages();
 

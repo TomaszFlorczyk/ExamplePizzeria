4462d52e0727a2f6dfe5d66ba12f9f2945724d5f
Tomasz Florczyk <tomasz.florczyk98@gmail.com>
2022-11-14 16:27:04 +0100

    Added pizza model

---
+++ Pizzeria/PizzeriaAPI/Migrations/20221114151802_PizzaModel.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzeriaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PizzaModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PizzaId",
+                table: "Ingredients",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Pizzas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pizzas", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_PizzaId",
+                table: "Ingredients",
+                column: "PizzaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredients_Pizzas_PizzaId",
+                table: "Ingredients",
+                column: "PizzaId",
+                principalTable: "Pizzas",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredients_Pizzas_PizzaId",
+                table: "Ingredients");
+
+            migrationBuilder.DropTable(
+                name: "Pizzas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ingredients_PizzaId",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "PizzaId",
+                table: "Ingredients");
+        }
+    }
+}

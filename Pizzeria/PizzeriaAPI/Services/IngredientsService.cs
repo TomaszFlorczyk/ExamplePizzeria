0af40e166bd56bd15302ac5127a79709465e1147
Tomasz Florczyk <tomasz.florczyk98@gmail.com>
2022-11-15 10:46:14 +0100

    Cos

---
+++ Pizzeria/PizzeriaAPI/Services/IngredientsService.cs
@@ -0,0 +1,61 @@
+﻿using PizzeriaAPI.Data;
+using PizzeriaAPI.Services.Interface;
+using PizzeriaShared.Models;
+
+namespace PizzeriaAPI.Services
+{
+    public class IngredientsService : IIngredientService
+    {
+        private ApplicationDbContext _applicationDbContext;
+
+        public IngredientsService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<Ingredient> CreateIngredient(string name)
+        {
+            // string.IsNullOrEmpty
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                return null!;
+            }
+
+            try
+            {
+                var ingredientToBeAdded = new Ingredient
+                {
+                    Name = name,
+                };
+
+                _ = await _applicationDbContext.Ingredients!.AddAsync(ingredientToBeAdded);
+                _ = await _applicationDbContext.SaveChangesAsync();
+
+                return ingredientToBeAdded;
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+
+
+        }
+        public Task<Ingredient> GetIngredientById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Ingredient> GetIngredientByName(string name)
+        {
+             throw new NotImplementedException();
+        }
+        public Task<Ingredient> DeleteIngredient(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        
+    }
+}
---
+++ Pizzeria/PizzeriaAPI/Services/Interface/IIngredientService.cs
@@ -0,0 +1,12 @@
+﻿using PizzeriaShared.Models;
+
+namespace PizzeriaAPI.Services.Interface
+{
+    public interface IIngredientService
+    {
+        public Task<Ingredient> CreateIngredient(string name);
+        public Task<Ingredient> GetIngredientById(int Id);
+        public Task<Ingredient> GetIngredientByName(string name);
+        public Task<Ingredient> DeleteIngredient(int Id);
+    }
+}
